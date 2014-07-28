e72ee958f8a4fe0c1c1d652c4199f6183d89e5c6
Panagiotis <sainis29@hotmail.com>
2014-07-28 18:00:26 +0000

    prokopis

--- app.js
+++ app.js
@@ -14,6 +14,25 @@ app.get('/', function(req, res) {
     res.render('maintest');
 });
 
+app.get('/MyRequests', function(req, res) {
+    res.render('myRequests');
+});
+
+app.get('/NewRequest', function(req, res) {
+    res.render('newRequest');
+});
+
+app.get('/NewRequestForm', function(req, res) {
+     console.log(req.body);
+    console.log(req.params);
+    console.log(req.query);
+    res.render('newRequestForm',{address: req.query.address});
+});
+
+app.get('/PreviewRequests', function(req, res) {
+    res.render('previewRequests');
+});
+
 app.get('/paramget/:name', function(req, res) {
     console.log(req.body);
     console.log(req.params);
